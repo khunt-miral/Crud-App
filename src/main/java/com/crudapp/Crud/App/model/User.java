0eee6bcde5a840a3fb4a3fd762d49b9bd0e70bc1
Khunt Miral <20ce051.miral.khunt@vvpedulink.ac.in>
2022-09-07 23:00:37 +0530

    Update User.java

--- src/main/java/com/crudapp/Crud/App/model/User.java
+++ src/main/java/com/crudapp/Crud/App/model/User.java
@@ -17,9 +17,18 @@ public class User {
     @Id
     private String id;
 
+    @NonNull
     private String firstName;
+    
+    @NonNull
     private String lastName;
+    
+    @NonNull
     private String mobileno;
+    
+    @NonNull
     private String username;
+    
+    @NonNull
     private String password;
 }
